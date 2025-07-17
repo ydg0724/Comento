a8771085abfca4947145bd00fedc8ae79da8fd5e
Yang <ydg0724@gmail.com>
2025-07-17 18:32:45 +0900

    spring boot 추가

---
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/ComentoStatisticApplication.java
@@ -0,0 +1,13 @@
+package com.demo.comentoStatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentoStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComentoStatisticApplication.class, args);
+	}
+
+}
---
+++ comentoStatistic/src/test/java/com/demo/comentoStatistic/ComentoStatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.comentoStatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComentoStatisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
